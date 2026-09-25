8f7861cb1e3c567797a111ed6a47daaafc66bc04
Akash Barman <1998akashbarman@gmail.com>
2026-09-25 20:29:52 +0530

    all code in python

---
+++ app.py
@@ -0,0 +1,189 @@
+# below is program for the addtion of the given two value by user
+
+# first_number = int(input("Enter your first number: "))
+# Second_number = int(input("Enter your second number: "))
+
+# sum = first_number + Second_number
+
+# print(sum)
+
+
+# performing the condition task by IF condition 
+
+# number = int(input("Enter your number which is less then 100: "))
+# if number < 100:
+#     print("you are are doing great job")
+# else:
+#     print("you are not doing good job")   
+#   
+
+
+# here below is the example of the multiplications of given values
+ 
+
+# first_number = 200
+# second_number = 600
+
+# sum = first_number + second_number
+# print(sum)
+
+# first_number = int(input("Enter your fisrt number: "))
+# second_number = int(input("Enter your second number: "))
+
+# sum = first_number + second_number
+
+# print(sum)
+
+# first_number = int(input("Enter your fisrt number: "))
+# second_number = int(input("Enter your second number: "))
+
+# sum = first_number / second_number
+
+# print(sum)
+
+# first_number = int(input("Enter your fisrt number: "))
+# second_number = int(input("Enter your second number: "))
+
+# sum = first_number * second_number
+
+# print(sum)
+
+# first_number = int(input("Enter your fisrt number: "))
+# second_number = int(input("Enter your second number: "))
+
+# sum = first_number - second_number
+
+# print(sum)
+
+
+
+# this below is the example of the finding the type of the variable like string float bool and integer  
+
+
+# number = True
+# print(type(number))
+
+# number = False
+# print(type(number))
+
+
+# this below is the example of concatenation
+
+
+# value = "hey you are so awesomn "
+
+# print(value + "Akash")
+
+
+# val = "Hi ! i am here only for Frontend Developer role "
+# val_next = " - Okay, Your most welcome for that position"
+
+# print(val + val_next)
+
+
+# task
+
+# Add a person with firs name is Tony a secound name is Stark
+
+# first_name = "Tony"
+# scound_name = "Stark"
+
+# print(first_name +" " + scound_name)
+
+
+#  Tony's age is 53
+
+# numer = 53
+
+# print("Tony's age is ",numer)
+
+
+#  Tony's height is 1.85
+# height = 1.85
+# print("Tony's height is ", height )
+
+
+# Tony is a secratly superhero. Take his  superhero name as input and print it 
+
+# text = str(input())
+# print("Tony's is a secratly ", text)
+
+# age = int(input("Enter your age "))
+
+# print(age, type(age))
+
+
+# age = input("Enter your age ")
+
+# new_age = int(age) + 5
+
+# print(new_age)
+
+
+# using the try and except with Value Error
+
+# try:
+#     age = int(input("Enter your age then i will added 5 years on that age: "))
+#     new_age = age + 5
+#     print(new_age)
+# except ValueError:
+#     print("Could you please enter right value")  
+# 
+
+
+# name ="My name is Akash Barman"
+
+# print(name.upper())
+
+#this below code is for finding the index value there is "Bar" so it will get index value 
+# where it is starting and when the value is not existing so there we will get -1 so that is the main concept 
+
+# name = "My name is Akash Barman"
+
+# print(name.find("Bar"))
+ 
+
+# replace operation 
+
+
+# name = "hey my name is akash barman"
+
+# print(name.replace("hey my name is akash barman", "hey you are not the akash barman because you are a genius"))
+
+# name = "hey my name is akash barman"
+
+# print(name.replace("barman", "hey you are not the akash barman because you are a genius"))
+# print("m" in name)  #this one line is checking the absence 
+
+# Task
+
+
+# Take price of 3 products as input (eg - 99.5, 23.75, 16.15)
+# print the total Bill amount
+# print the average price
+# Take a superhero name as input & check if it starts with 'S' / 's' or not.
+
+
+# first_prod = -99.5
+# sec_prod = 23.75
+# third_prod = 16.15
+
+# sum = first_prod + sec_prod + third_prod
+# avarage = sum / 3
+# print("Total bill aount is: ", sum)
+# print("The avarage price is: ", avarage)
+
+# name = "superhero"
+# print("s" in name)   #in this case we will get s and output we will get true
+# print("S" in name)   #in this case we won't get the S so output we will get false 
+
+
+
+
+
+
+
+
+
+
+
